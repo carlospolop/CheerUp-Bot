ffb9637c8814ad96f10e439a0379c938273611a5
carlospolop <carlospolop@gmail.com>
2017-06-10 17:11:53 +0200

    Add files via upload

---
+++ classes/texto_a_array.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+import requests
+from random import randint
+from bs4 import BeautifulSoup
+import re
+
+class Texto_a_array:
+    def __init__(self, text_path):
+        self.text_path = text_path
+        self.text_array = []
+
+    def load_textArray(self):
+        print "Cargando array de texto de: "+self.text_path
+        f = open(self.text_path, "r")
+        for line in f:
+            self.text_array.append(line)
+
+    def get_comment(self):
+        if not self.text_array:
+            return "Empty:("
+        index = randint(0,len(self.text_array)-1)
+        return self.text_array[index]
